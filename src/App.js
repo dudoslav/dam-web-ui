6f030cc449b5f7848c92949afe4e2563a173f385
Dusan Baran <work.dusanbaran@gmail.com>
2018-12-03 17:31:48 +0000

    Add basic header

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1>DAM</h1>
+          <img src={logo} width="100" height="100" alt="DAM logo"/>
         </header>
+        <DownloadList/>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+class DownloadList extends Component {
+  constructor(props) {
+    super(props)
+    this.state =
+    { downloads:
+      [{ name: 'test1', progress: 42 },
+       { name: 'test2', progress: 0 },
+       { name: 'test3', progress: 100 }] }
+  }
+
+
+  render() {
+    const listItems = this.state.downloads.map((download, index) => <DownloadItem download={download} key={index}/> )
+    return (
+      <ul>
+        {listItems}
+      </ul>
+    )
+  }
+}
+
+function DownloadItem(props) {
+  return (
+    <li>
+      <details>
+        <summary>
+          <span>{props.download.name}</span>
+          <button>&#x00d7;</button>
+          <progress value={props.download.progress} max="100"></progress>
+        </summary>
+        <div>
+          DETAILS
+        </div>
+      </details>
+    </li>
+  )
+}
+
+export default App
